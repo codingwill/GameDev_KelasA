2ed9d724bac52ca4838ebfdac33afb6208bace77
wkwkwill <willykomara@gmail.com>
2019-12-08 12:58:17 +0700

    Add project files.

---
+++ GameDev_KelasA/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Konsep Pemrograman - Game Dev C++");
+
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.display();
+	}
+
+	return 0;
+}
