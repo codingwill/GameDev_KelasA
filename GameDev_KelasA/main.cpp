162b6c5afc69458273e892851cc0a6ba0614c4a1
wkwkwill <willykomara@gmail.com>
2019-12-12 15:54:46 +0700

    Animasi + Kontrol

--- GameDev_KelasA/main.cpp
+++ GameDev_KelasA/main.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 using namespace sf;
 
+
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Konsep Pemrograman - Game Dev C++");
 	Texture idle0;
 	idle0.loadFromFile("images/ninja/png/Idle__000.png");
-	
-	/*
 	Texture idle1;
 	idle1.loadFromFile("images/ninja/png/Idle__001.png");
 	Texture idle2;
@@ -29,21 +29,43 @@ int main()
 	idle8.loadFromFile("images/ninja/png/Idle__008.png");
 	Texture idle9;
 	idle9.loadFromFile("images/ninja/png/Idle__009.png");
-	*/
+	
+	Texture Run0;
+	Run0.loadFromFile("images/ninja/png/Run__000.png");
+	Texture Run1;
+	Run1.loadFromFile("images/ninja/png/Run__001.png");
+	Texture Run2;
+	Run2.loadFromFile("images/ninja/png/Run__002.png");
+	Texture Run3;
+	Run3.loadFromFile("images/ninja/png/Run__003.png");
+	Texture Run4;
+	Run4.loadFromFile("images/ninja/png/Run__004.png");
+	Texture Run5;
+	Run5.loadFromFile("images/ninja/png/Run__005.png");
+	Texture Run6;
+	Run6.loadFromFile("images/ninja/png/Run__006.png");
+	Texture Run7;
+	Run7.loadFromFile("images/ninja/png/Run__007.png");
+	Texture Run8;
+	Run8.loadFromFile("images/ninja/png/Run__008.png");
+	Texture Run9;
+	Run9.loadFromFile("images/ninja/png/Run__009.png");
+
 	Sprite player;
 	player.setScale(1.0f, 1.0f);
-	player.setTexture(idle0);
-	/*
+	player.setOrigin(116.0f, 219.5f);
+	//player.setTexture(idle0);
+	
 	//SET PENGHITUNG WAKTU
 	Clock clock;
 	Time time;
+
 	float currentTime;
 	float fps = 0.1f;
-	*/
+	
 	// run the program as long as the window is open
 	while (window.isOpen())
 	{
-		/*
 		time = clock.getElapsedTime();
 		currentTime = time.asSeconds();
 		player.setTexture(idle0, true);
@@ -73,21 +95,46 @@ int main()
 
 		if (Keyboard::isKeyPressed(Keyboard::W))
 		{
-			player.setPosition(player.getPosition().x + 0, player.getPosition().y + 1);
+			player.setPosition(player.getPosition().x + 0, player.getPosition().y - 1);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::D))
 		{
 			player.setPosition(player.getPosition().x + 1, player.getPosition().y + 0);
+			//player.setRotation(player.getRotation() + 1.0f);
+			//player.setTexture(Run0, true);
+			//SET ANIMASI
+			if (currentTime >= 10 * fps)
+				clock.restart();
+			else if (currentTime >= 9 * fps)
+				player.setTexture(Run9);
+			else if (currentTime >= 8 * fps)
+				player.setTexture(Run8);
+			else if (currentTime >= 7 * fps)
+				player.setTexture(Run7);
+			else if (currentTime >= 6 * fps)
+				player.setTexture(Run6);
+			else if (currentTime >= 5 * fps)
+				player.setTexture(Run5);
+			else if (currentTime >= 4 * fps)
+				player.setTexture(Run4);
+			else if (currentTime >= 3 * fps)
+				player.setTexture(Run3);
+			else if (currentTime >= 2 * fps)
+				player.setTexture(Run2);
+			else if (currentTime >= 1 * fps)
+				player.setTexture(Run1);
+			else if (currentTime >= 0 * fps)
+				player.setTexture(Run0);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::S))
 		{
-			player.setPosition(player.getPosition().x + 0, player.getPosition().y - 1);
+			player.setPosition(player.getPosition().x + 0, player.getPosition().y + 1);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::A))
 		{
 			player.setPosition(player.getPosition().x - 1, player.getPosition().y + 0);
 		}
-		*/
+
 		// check all the window's events that were triggered since the last iteration of the loop
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -96,8 +143,8 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
 		window.draw(player);
-		
 		window.display();
 	}
 
